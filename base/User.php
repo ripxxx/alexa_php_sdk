49383f644a351c42ead1f7b10b5c334ea382ddfe
Alexander Berdnikov <sasha@onix-systems.com>
2016-07-28 13:40:51 +0300

    user session fix, now every user+skill have own session file, with permanent session data, current and all previous sessions

--- base/User.php
+++ base/User.php
@@ -10,129 +10,90 @@ class User implements ArrayAccess, Countable {
     protected $fileName;
     protected $hash;
     protected $path;
-    protected $private;
 
-    protected function __construct($id, $path, $applicationId, $sessionId, $private) {
+    protected function __construct($id, $path, $applicationId, $sessionId) {
         $this->currentApplicationId = $applicationId;
         $this->currentSessionId = $sessionId;
         $this->id = $id;
-        $this->hash = md5($id.'com.alexa.sdk');
+        $this->hash = md5($applicationId.$id.'com.alexa.sdk');
         $this->path = $path;
-        $this->private = $private;
         
         $this->fileName = $this->path.$this->hash;
         $this->data = array();
         if(file_exists($this->fileName)) {
             $serializedData = file_get_contents($this->fileName);
             $this->data = unserialize($serializedData);
-            if(!isset($this->data[$this->currentApplicationId])) {
-                $this->data[$this->currentApplicationId] = array();
+            if(!is_array($this->data)) {
+                $this->data = array();
             }
             
-            if(!isset($this->data[$this->currentApplicationId][$this->currentSessionId])) {
-                $this->data[$this->currentApplicationId][$this->currentSessionId] = array();
+            if(!isset($this->data[$this->currentSessionId])) {
+                $this->data[$this->currentSessionId] = array();
             }
-            
-            $this->data[$this->currentApplicationId][$this->currentSessionId]['private'] = $this->private;
         }
         else {
             $this->data = [
-                $this->currentApplicationId => [
-                    $this->currentSessionId => [
-                        'private' => $this->private
-                    ]
-                ],
+                $this->hash => [],
+                $this->currentSessionId => [],
             ];
         }
     }
     
     public function __destruct() {
+        foreach($this->data[$this->currentSessionId] as $key=>$value) {
+           $this->data[$this->hash][$key] = $value;
+        }
         $serializedData = serialize($this->data);
         file_put_contents($this->fileName, $serializedData);
     }
     
     public function __get($name) {
-        if(($name == 'session') && isset($this->data[$this->currentApplicationId][$this->currentSessionId])) {
-            $data = $this->data[$this->currentApplicationId][$this->currentSessionId];
-            if(array_key_exists('private', $data)) {
-                unset($data['private']);
-            }
+        if(($name == 'session') && isset($this->data[$this->currentSessionId])) {
+            $data = $this->data[$this->currentSessionId];
             return $data;
         }
         return NULL;
     }
     
     public function offsetExists($offset) {
-        if(isset($this->data[$offset])) {
-            if($offset == $this->currentApplicationId) {
-                return true;
-            }
-            foreach($this->data[$offset] as $sessionId=>$data) {
-                if(!$this->data[$offset][$sessionId]['private']) {
-                    return true;
-                }
-            }
-        }
-        else if(isset($this->data[$this->currentApplicationId][$offset])) {
-            if($offset == $this->currentSessionId) {
-                return true;
-            }
-            //return !$this->data[$this->currentApplicationId][$offset]['private'];
-            return isset($this->data[$this->currentApplicationId][$offset]);
-        }
-        
-        return isset($this->data[$this->currentApplicationId][$this->currentSessionId][$offset]);
+        return isset($this->data[$this->currentSessionId][$offset]) || isset($this->data[$this->hash][$offset]);
     }
 
     public function offsetGet($offset) {
-        if(isset($this->data[$offset])) {
-            if($offset == $this->currentApplicationId) {
-                return $this->data[$offset];
-            }
-            $data = array();
-            foreach($this->data[$offset] as $sessionId=>$data) {
-                if(!$this->data[$offset][$sessionId]['private']) {
-                    $data[$sessionId] = $data;
-                }
-            }
-            if(count($data) > 0) {
-               return $data; 
-            }
-            return NULL;
+        if(isset($this->data[$this->currentSessionId][$offset])) {
+            return $this->data[$this->currentSessionId][$offset];
         }
-        else if(isset($this->data[$this->currentApplicationId][$offset])) {
-            if($offset == $this->currentSessionId) {
-                return $this->data[$this->currentApplicationId][$offset];
-            }
-            //return ((!$this->data[$this->currentApplicationId][$offset]['private'])? $this->data[$this->currentApplicationId][$offset]: NULL);
-            return ((isset($this->data[$this->currentApplicationId][$offset]))? $this->data[$this->currentApplicationId][$offset]: NULL);
+        else if(isset($this->data[$this->hash][$offset])) {
+            return $this->data[$this->hash][$offset];
         }
-        
-        return ((isset($this->data[$this->currentApplicationId][$this->currentSessionId][$offset]))? $this->data[$this->currentApplicationId][$this->currentSessionId][$offset]: NULL);
+        return NULL;
     }
 
     public function offsetSet($offset, $value) {
-        if(!isset($this->data[$offset]) && !isset($this->data[$this->currentApplicationId][$offset])) {
-            if (is_null($offset)) {
-                $this->data[$this->currentApplicationId][$this->currentSessionId][] = $value;
-            }
-            else {
-                $this->data[$this->currentApplicationId][$this->currentSessionId][$offset] = $value;
-            }
+        if (is_null($offset)) {
+            $this->data[$this->currentSessionId][] = $value;
+        }
+        else {
+            $this->data[$this->currentSessionId][$offset] = $value;
         }
     }
 
     public function offsetUnset($offset) {
-        if(!isset($this->data[$offset]) && !isset($this->data[$this->currentApplicationId][$offset]) && isset($this->data[$this->currentApplicationId][$this->currentSessionId][$offset])) {
-            unset($this->data[$this->currentApplicationId][$this->currentSessionId][$offset]);
+        if(isset($this->data[$this->currentSessionId][$offset])) {
+            unset($this->data[$this->currentSessionId][$offset]);
+        }
+        
+        if(isset($this->data[$this->hash][$offset])) {
+            unset($this->data[$this->hash][$offset]);
         }
     }
     
     public function count() {
-        return count($this->data);
+        $cnt = count($this->data[$this->currentSessionId])+count(array_diff($this->data[$this->hash], $this->data[$this->currentSessionId]));
+        return $cnt;
     }
     
-    public static function getInstance($id = NULL, $path = NULL, $applicationId = NULL, $sessionId = NULL, $private = true) {
+    public static function getInstance($id = NULL, $path = NULL, $applicationId = NULL, $sessionId = NULL) {
         if(self::$instance) {
             return self::$instance;
         }
@@ -146,7 +107,7 @@ class User implements ArrayAccess, Countable {
                     $error = 'Directory is not writable: "'.path.'".';
                     throw new Exception($error);
                 }
-                self::$instance = new self($id, $path, $applicationId, $sessionId, $private);
+                self::$instance = new self($id, $path, $applicationId, $sessionId);
                 return self::$instance;
             }
             else if(strlen($id) == 0) {
